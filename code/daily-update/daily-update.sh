eef0ced870ba1dcefcb6973f33f0b174f4d0acd1
Rich Trott <rtrott@gmail.com>
2022-03-16 17:37:04 -0700

    chore: update files

---
+++ code/daily-update/daily-update.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+open https://ucsf.app.box.com/notes/913902401936
+open https://ucsflibrary.zendesk.com/explore/#/pivot-table/connection/10666801/query/54636391
+open https://ucsf--c.visualforce.com/apex/Recog_Site_Page
